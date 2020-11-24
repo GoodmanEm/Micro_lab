0103aebb0d2d93a7323337e02dccd80f298b4077
GoodmanEm <67795346+GoodmanEm@users.noreply.github.com>
2020-11-23 20:12:41 -0500

    Update Lab06-1.c

--- Lab06-1.c
+++ Lab06-1.c
@@ -15,7 +15,7 @@ USBH_HandleTypeDef husbh;
 //int8_t y;
 // uint8_t buttons[3];
 
-Diskio_drvTypeDef FF;
+//Diskio_drvTypeDef FF;
 
 void USBH_Process_code(USBH_HandleTypeDef *, uint8_t); 	// USING CN15
 void initalize_pins();
@@ -34,10 +34,9 @@ uint8_t Appli_state = 0;
 int main(void){
 	 // System Initializations
 	Sys_Init();		//USB_OTG_HS	// USBD_RegisterClass ()
-	initalize_pins();
 
     // Application Initializations
-	FATFS_LinkDriver(&FF,path);
+	//FATFS_LinkDriver(&FF,path);
 
 	// USBH Driver Initialization	//HID Joystick mouse usbh_hid(.c,.h HID class state handler.
     USBH_Init(&husbh, USBH_Process_code, 0);
@@ -138,45 +137,3 @@ void USB_HID_Callback(USBH_HandleTypeDef *phost, uint8_t id){
 
 	}
 }
-
-
-
-void initalize_pins(){
-	/*
-	PA3 USB_OTG_HS_ULPI_D0
-	PA5 USB_OTG_HS_ULPI_CK
-	PB0 USB_OTG_HS_ULPI_D1
-	PB1 USB_OTG_HS_ULPI_D2
-	PB5 USB_OTG_HS_ULPI_D7
-	PB10 USB_OTG_HS_ULPI_D3
-	PB11 USB_OTG_HS_ULPI_D4
-	PB12 USB_OTG_HS_ULPI_D5
-	PB13 USB_OTG_HS_ULPI_D6
-	PC0 USB_OTG_HS_ULPI_STP
-	PH4 USB_OTG_HS_ULPI_NXT
-	PI11 USB_OTG_HS_ULPI_DIR*/
-
-	GPIO_InitTypeDef GPIOA_InitStruct;
-	GPIO_InitTypeDef GPIOB_InitStruct;
-	GPIO_InitTypeDef GPIOC_InitStruct;
-	GPIO_InitTypeDef GPIOH_InitStruct;
-	GPIO_InitTypeDef GPIOI_InitStruct;
-
-/*	USB_OTG_HS_ULPI_D0.Pin = GPIO_PIN_3;*/
-
-	GPIOA_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_5;
-	HAL_GPIO_Init(GPIOA, &GPIOA_InitStruct);
-
-	GPIOB_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_5|GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12 |GPIO_PIN_13;
-	HAL_GPIO_Init(GPIOB, &GPIOB_InitStruct);
-
-	GPIOC_InitStruct.Pin = GPIO_PIN_0;
-	HAL_GPIO_Init(GPIOC, &GPIOC_InitStruct);
-
-	GPIOH_InitStruct.Pin = GPIO_PIN_4;
-	HAL_GPIO_Init(GPIOH, &GPIOH_InitStruct);
-
-	GPIOI_InitStruct.Pin = GPIO_PIN_11;
-	HAL_GPIO_Init(GPIOI, &GPIOI_InitStruct);
-}
-
